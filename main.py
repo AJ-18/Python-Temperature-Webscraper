5abfb829593b8d5bf5ed30b173de367325dc25ff
AJ-18 <arjun.suresh1@marist.edu>
2026-04-15 09:49:20 -0400

    Temp webscrape + timestamp store

---
+++ main.py
@@ -0,0 +1,41 @@
+import streamlit
+import requests
+import selectorlib
+from datetime import datetime
+
+URL = "https://programmer100.pythonanywhere.com"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response =  requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["temp"]
+    return value
+
+def timestamp():
+    now = datetime.now()
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return current_time
+
+def store(extracted, time):
+    with open("data.txt", "a") as file:
+        file.write(time + ", " + extracted + "\n")
+
+def read():
+    with open("data.txt", "r") as file:
+        return file.read()
+
+if __name__ == "__main__":
+    scraped_info = scrape(URL)
+    extracted = extract(scraped_info)
+    content = read()
+    if extracted not in content:
+        time = timestamp()
+        store(extracted, time)
+    print(timestamp() + " " + extracted)
